ed90d0d26d5f9e3f30e868bff396c2f35d72eb10
Maximilian Petkov <75382505+max-petkov@users.noreply.github.com>
2021-12-20 23:22:48 +0200

    Form Script

--- main.js
+++ main.js
@@ -1,32 +1,96 @@
 $(function () {
-  const $sectorContainer = $(".sector-value");
-  const $sectorCheckBoxList = $(".sector-list");
-  const $sectorCheckBoxes = $('.find-job__sector [type="checkbox"]');
-  const $form = $(".find-job__form");
+  // Variables
+  const $showCheckboxes = $(".js-slide-checkbox");
+  const $checkboxLists = $(".checkbox-list");
+  const $sectorCheckboxes = $('.checkbox-list--sector [type="checkbox"]');
+  const $levelCheckboxes = $('.checkbox-list--level [type="checkbox"]');
+  const $languageCheckboxes = $('.checkbox-list--language [type="checkbox"]');
+  const $cityCheckboxes = $('.checkbox-list--city [type="checkbox"]');
+  const $employmentTypeCheckboxes = $(
+    '.checkbox-list--employment-type [type="checkbox"]'
+  );
 
-  $sectorContainer.on("click", () => $sectorCheckBoxList.slideToggle("fast"));
-});
+  // Functions
+  function $countCheckboxes($el, $arr) {
+    const $checkbox = $el;
+    const $counter = $checkbox
+      .parents(".checkbox-list")
+      .siblings(".form__input")
+      .find(".js-count-checked-checkboxes");
+    const $inputText = $checkbox
+      .parents(".checkbox-list")
+      .siblings(".form__input")
+      .find("p");
 
-const sectorCheckBoxes = document.querySelectorAll(
-  '.find-job__sector [type="checkbox"]'
-);
-const displayCheckboxValue = document.querySelector(".sector-value p");
-const emptyArray = [];
-
-sectorCheckBoxes.forEach((el, i) => {
-  el.addEventListener("change", function () {
-    if (el.checked) {
-      emptyArray.push(el.value);
-      displayCheckboxValue.textContent = `${emptyArray.join(", ")}`;
-
-      if (!emptyArray.length) displayCheckboxValue.textContent = "Сектор";
+    if ($checkbox.is(":checked")) {
+      $arr.push($checkbox.val());
     }
 
-    if (!el.checked) {
-      emptyArray.splice(emptyArray.indexOf(el.value), 1);
-      displayCheckboxValue.textContent = `${emptyArray.join(", ")}`;
+    if (!$checkbox.is(":checked"))
+      $arr.splice($arr.indexOf($checkbox.val()), 1);
 
-      if (!emptyArray.length) displayCheckboxValue.textContent = "Сектор";
+    if (!$arr.length) {
+      $inputText.css("color", "#b2b6bd");
+      $counter.html("");
+    } else {
+      $inputText.css("color", "#000");
+      $counter.html(`(${$arr.length})`);
     }
+  }
+
+  // Show checkboxes
+  $showCheckboxes.on("click", function () {
+    const $checkboxContainer = $(this).siblings(".checkbox-list");
+    const $chevron = $(this).find("img");
+
+    if ($checkboxContainer.is(":visible")) {
+      $chevron.removeClass("chevron--active");
+      $checkboxContainer.slideUp("fast");
+    } else {
+      $checkboxContainer.slideDown("fast");
+      $chevron.addClass("chevron--active");
+    }
+  });
+
+  // Hide elements when click outside
+  $(document).on("mouseup", function (e) {
+    if (
+      !$(e.target).closest($checkboxLists).length &&
+      $($checkboxLists).is(":visible")
+    ) {
+      $($checkboxLists).slideUp("fast");
+      $(".chevron").removeClass("chevron--active");
+    }
+  });
+
+  // Count Checkboxes
+  const $sectorArray = [];
+  $sectorCheckboxes.on("change", function () {
+    const $checkbox = $(this);
+    $countCheckboxes($checkbox, $sectorArray);
+  });
+
+  const $levelArray = [];
+  $levelCheckboxes.on("change", function () {
+    const $checkbox = $(this);
+    $countCheckboxes($checkbox, $levelArray);
+  });
+
+  const $languageArray = [];
+  $languageCheckboxes.on("change", function () {
+    const $checkbox = $(this);
+    $countCheckboxes($checkbox, $languageArray);
+  });
+
+  const $cityArray = [];
+  $cityCheckboxes.on("change", function () {
+    const $checkbox = $(this);
+    $countCheckboxes($checkbox, $cityArray);
+  });
+
+  const $employmentTypeArray = [];
+  $employmentTypeCheckboxes.on("change", function () {
+    const $checkbox = $(this);
+    $countCheckboxes($checkbox, $employmentTypeArray);
   });
 });
