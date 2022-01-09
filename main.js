1635d030dcc876e18aa36cef8a2c61a84199bbee
unknown <maxy.dp@abv.bg>
2022-01-09 23:09:55 +0200

    added checkbox & radio btns style, reset IT sector, etc...

--- main.js
+++ main.js
@@ -24,12 +24,11 @@ $(function () {
       .siblings(".form__input")
       .find("p");
 
-    if ($checkbox.is(":checked")) {
-      $arr.push($checkbox.val());
-    }
+    if ($checkbox.is(":checked")) $arr.push($checkbox.val());
 
-    if (!$checkbox.is(":checked"))
+    if (!$checkbox.is(":checked")) {
       $arr.splice($arr.indexOf($checkbox.val()), 1);
+    }
 
     if (!$arr.length) {
       $inputText.css("color", "#b2b6bd");
@@ -42,29 +41,38 @@ $(function () {
 
   // Show checkboxes
   $showDropdownContent.on("click", function () {
+    const $dropDown = $(this);
     const $dropDownContent = $(this).siblings(".js-dropdown-content");
     const $chevron = $(this).find("img");
 
     if ($dropDownContent.is(":visible")) {
       $chevron.removeClass("chevron--active");
       $dropDownContent.slideUp("fast");
+      $dropDown.removeClass("border-bottom-radius-none");
     } else {
       $dropDownContent.slideDown("fast");
       $chevron.addClass("chevron--active");
+      $dropDown.addClass("border-bottom-radius-none");
     }
   });
 
-   // Show Technology when IT Sektor is checked
-   $('[name="it-sector"]').on('change', function() {
+  // Show Technology when IT Sektor is checked
+  $('[name="it-sector"]').on("change", function () {
     const $checkbox = $(this);
-    const $sectorITContainer = $('.sector-container--it');
+    const $sectorITContainer = $(".sector-container--it");
 
-    if($checkbox.is(':checked')) $sectorITContainer.removeClass('d-none');
-    else  {
-      // TODO remove all checked checkboxes
-      $sectorITContainer.addClass('d-none');
-    };
-  })
+    if ($checkbox.is(":checked")) $sectorITContainer.fadeIn("slow");
+    else {
+      $sectorITContainer.fadeOut("slow");
+      const $checkboxesIT = $(".checkbox-list--sector-it [type='checkbox']");
+
+      $checkboxesIT.prop("checked", function (i, val) {
+        $checkboxesIT[i].checked = false;
+      });
+      $(".form__input--sector-it p").css("color", "#b2b6bd");
+      $(".form__input--sector-it p .js-count-checked-checkboxes").html("");
+    }
+  });
 
   // Hide elements when click outside
   $(document).on("mouseup", function (e) {
@@ -74,6 +82,31 @@ $(function () {
     ) {
       $(".js-dropdown-content").slideUp("fast");
       $(".chevron").removeClass("chevron--active");
+      $(".js-slide-dropdown").removeClass("border-bottom-radius-none");
+    }
+  });
+
+  // Fire tabindex
+  let $focusedDropdown;
+  $(".js-slide-dropdown").focus(function () {
+    $focusedDropdown = $(this);
+  });
+
+  $("body").on("keydown", function (e) {
+    if (e.target.classList.contains("js-slide-dropdown")) {
+      $(".js-slide-dropdown").each(($i, $el) => {
+        if ($el == document.activeElement) {
+          $focusedDropdown.siblings(".js-dropdown-content").slideDown("fast");
+        } else {
+          $el.nextElementSibling.style.display = "none";
+        }
+      });
+    }
+
+    if (!e.target.classList.contains("js-slide-dropdown")) {
+      $(".js-slide-dropdown").each(($i, $el) => {
+        $el.nextElementSibling.style.display = "none";
+      });
     }
   });
 
@@ -96,7 +129,11 @@ $(function () {
     $countCheckboxes($checkbox, $sectorArray);
   });
 
-  const $sectorITArray = [];
+  let $sectorITArray = [];
+  $('[name="it-sector"]').on("change", () => {
+    if (!$(this).checked) $sectorITArray = [];
+  });
+
   $sectorITCheckboxes.on("change", function () {
     const $checkbox = $(this);
     $countCheckboxes($checkbox, $sectorITArray);
